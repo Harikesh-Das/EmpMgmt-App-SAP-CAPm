8a07d398544372d3e6cdacdfcb773a8376197002
Harikesh Das <112227085+Harikesh-Das@users.noreply.github.com>
2026-07-22 15:08:45 +0530

    emp-service.js edit

--- srv/employee-service.js
+++ srv/employee-service.js
@@ -1,13 +1,48 @@
-import cds from '@sap/cds';
-import verifyToken from './lib/auth.js';
+import cds from "@sap/cds";
 
-export default cds.service.impl(async function () {
+export default cds.service.impl(function () {
+
     const { Employee } = this.entities;
 
-    this.before('READ', Employee, async (req) => {
-        
-        console.log(req.user);
-        
+    this.on("READ", Employee, async (req) => {
 
-    })
-})
+        if (req.user.is("HR")) {
+            return SELECT.from(Employee);
+        }
+
+        if (req.user.is("Manager")) {
+
+            const manager = await SELECT.one
+                .from(Employee)
+                .where({
+                    email: req.user.id
+                });
+
+            if (!manager) {
+                req.reject(403, "Manager record not found.");
+            }
+
+            return SELECT
+                .from(Employee)
+                .where({
+                    ID: manager.ID
+                })
+                .or({
+                    manager_ID: manager.ID
+                });
+        }
+
+        if (req.user.is("Employee")) {
+
+            return SELECT.one
+                .from(Employee)
+                .where({
+                    email: req.user.id
+                });
+        }
+
+        req.reject(403, "Forbidden");
+
+    });
+
+});
